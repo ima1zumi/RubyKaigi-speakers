fc22fe1ccce09b2be7746010a9ce4356880d18c5
Mari Imaizumi <mariimaizumi5@gmail.com>
2024-06-29 23:59:37 +0900

    Enable to download multiple files for a year

--- get_schedule.rb
+++ get_schedule.rb
@@ -1,29 +1,31 @@
 require 'open-uri'
 
-uris = [
-  'https://rubykaigi.org/2024/schedule/',
-  'https://raw.githubusercontent.com/ruby-no-kai/rubykaigi-static/master/2023/schedule/index.html',
-  'https://raw.githubusercontent.com/ruby-no-kai/rubykaigi-static/master/2022/schedule/index.html',
-  'https://raw.githubusercontent.com/ruby-no-kai/rubykaigi-static/master/2021-takeout/schedule/index.html',
-  'https://raw.githubusercontent.com/ruby-no-kai/rubykaigi-static/master/2020-takeout/schedule/index.html',
-  'https://raw.githubusercontent.com/ruby-no-kai/rubykaigi-static/master/2019/schedule/index.html',
-  'https://raw.githubusercontent.com/ruby-no-kai/rubykaigi-static/master/2018/schedule/index.html',
-  'https://raw.githubusercontent.com/ruby-no-kai/rubykaigi-static/master/2017/schedule/index.html',
-  'https://raw.githubusercontent.com/ruby-no-kai/rubykaigi-static/master/2016/schedule/index.html',
-  'https://raw.githubusercontent.com/ruby-no-kai/rubykaigi-static/master/2015/schedule/index.html',
-  'https://raw.githubusercontent.com/ruby-no-kai/rubykaigi-static/master/2014/schedule/index.html',
-  'https://raw.githubusercontent.com/ruby-no-kai/rubykaigi-static/master/2013/schedule/index.html',
-  'https://raw.githubusercontent.com/ruby-no-kai/rubykaigi-static/master/2011/en/schedule/grid/index.html',
-  'https://raw.githubusercontent.com/ruby-no-kai/rubykaigi-static/master/2010/en/timetable/index.html',
-  'https://raw.githubusercontent.com/ruby-no-kai/rubykaigi-static/master/2009/en/talks/index.html',
-  'https://raw.githubusercontent.com/ruby-no-kai/rubykaigi-static/master/2008/Program_en.html',
-  'https://raw.githubusercontent.com/ruby-no-kai/rubykaigi-static/master/2007/Program-EN.html',
-  'https://raw.githubusercontent.com/ruby-no-kai/rubykaigi-static/master/2006/program.html'
-]
+years = {
+  '2024' => ['https://rubykaigi.org/2024/schedule/'],
+  '2023' => ['https://raw.githubusercontent.com/ruby-no-kai/rubykaigi-static/master/2023/schedule/index.html'],
+  '2022' => ['https://raw.githubusercontent.com/ruby-no-kai/rubykaigi-static/master/2022/schedule/index.html'],
+  '2021-takeout' => ['https://raw.githubusercontent.com/ruby-no-kai/rubykaigi-static/master/2021-takeout/schedule/index.html'],
+  '2020-takeout' => ['https://raw.githubusercontent.com/ruby-no-kai/rubykaigi-static/master/2020-takeout/schedule/index.html'],
+  '2019' => ['https://raw.githubusercontent.com/ruby-no-kai/rubykaigi-static/master/2019/schedule/index.html'],
+  '2018' => ['https://raw.githubusercontent.com/ruby-no-kai/rubykaigi-static/master/2018/schedule/index.html'],
+  '2017' => ['https://raw.githubusercontent.com/ruby-no-kai/rubykaigi-static/master/2017/schedule/index.html'],
+  '2016' => ['https://raw.githubusercontent.com/ruby-no-kai/rubykaigi-static/master/2016/schedule/index.html'],
+  '2015' => ['https://raw.githubusercontent.com/ruby-no-kai/rubykaigi-static/master/2015/schedule/index.html'],
+  '2014' => ['https://raw.githubusercontent.com/ruby-no-kai/rubykaigi-static/master/2014/schedule/index.html'],
+  '2013' => ['https://raw.githubusercontent.com/ruby-no-kai/rubykaigi-static/master/2013/schedule/index.html'],
+  '2011' => ['https://raw.githubusercontent.com/ruby-no-kai/rubykaigi-static/master/2011/en/schedule/grid/index.html'],
+  '2010' => ['https://raw.githubusercontent.com/ruby-no-kai/rubykaigi-static/master/2010/en/timetable/index.html'],
+  '2009' => ['https://raw.githubusercontent.com/ruby-no-kai/rubykaigi-static/master/2009/en/talks/index.html'],
+  '2008' => ['https://raw.githubusercontent.com/ruby-no-kai/rubykaigi-static/master/2008/MainSession_en.html', 'https://raw.githubusercontent.com/ruby-no-kai/rubykaigi-static/master/2008/SubSession_en.html'],
+  '2007' => ['https://raw.githubusercontent.com/ruby-no-kai/rubykaigi-static/master/2007/Program-EN.html'],
+  '2006' => ['https://raw.githubusercontent.com/ruby-no-kai/rubykaigi-static/master/2006/program.html']
+}
 
-uris.each do |uri|
-  URI.open(uri) do |response|
-    year = uri.match(/\d{4}-takeout|\d{4}/)
-    IO.copy_stream(response, "schedule/#{year}.html")
+years.each do |year, uris|
+  Dir.mkdir('schedule/#{year}') unless Dir.exist?('schedule/#{year}')
+  uris.each do |uri|
+    URI.open(uri) do |response|
+      IO.copy_stream(response, uri)
+    end
   end
 end

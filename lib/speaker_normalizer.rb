f01cb7072450b0db4bae403bd30da59876146738
Akira Matsuda <ronnie@dio.jp>
2024-08-26 22:54:00 +0900

    藤本尚邦 => Fujimoto Hisa
    2006	藤本尚邦
    2007	Fujimoto Hisa

--- lib/speaker_normalizer.rb
+++ lib/speaker_normalizer.rb
@@ -4,6 +4,8 @@ class SpeakerNormalizer
     case name
     when "江渡 浩一郎"
       "Kouichirou ETO"
+    when "藤本尚邦"
+      "Fujimoto Hisa"
     when "Naoto TAKAI"
       "Naoto Takai"
     when "Yuichi TATENO"
